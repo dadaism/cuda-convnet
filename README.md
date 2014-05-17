5fe3b25f619e10e131395fc860f6248f659254ba
daniel <daniel@nps5.(none)>
2014-05-17 16:13:40 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+This is a copy of the ``cuda-convnet`` convolutional neural network
+implementation written by Alex Krizhevsky.
+
+``cuda-convnet`` has quite extensive documentation itself.  Find the
+`MAIN DOCUMENTATION HERE <http://code.google.com/p/cuda-convnet/>`_.
+
+===================
+Additional features
+===================
+
+This document will have several reading notes of mine for
+``cuda-convnet`` as hosted on Google Code and this version.
+
+Updated to work with CUDA 5.5
+=============================
+
+The build configuration and code has been updated to work with CUDA
+5.5.  The paths in ``build.sh`` and ``Makefile`` were updated to work
+with the default install directories of the `CUDA Toolkit 5.5
+<https://developer.nvidia.com/cuda-downloads>`_.
+
--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # Fill in these environment variables.
-# I have tested this code with CUDA 4.0, 4.1, and 4.2. 
+
+# This file and others have been updated to work with CUDA 5.0.
+
 # Only use Fermi-generation cards. Older cards won't work.
 
 # If you're not sure what these paths should be, 
@@ -14,19 +16,19 @@
 # (it'll almost certainly be under /usr)
 
 # CUDA toolkit installation directory.
-export CUDA_INSTALL_PATH=/usr/local/cuda
+export CUDA_INSTALL_PATH=/usr/local/cuda-5.5
 
 # CUDA SDK installation directory.
-export CUDA_SDK_PATH=/home/spoon/NVIDIA_GPU_Computing_SDK
+export CUDA_SDK_PATH=$CUDA_INSTALL_PATH
 
 # Python include directory. This should contain the file Python.h, among others.
-export PYTHON_INCLUDE_PATH=/usr/include/python2.7
+export PYTHON_INCLUDE_PATH=/usr/include/python2.6
 
 # Numpy include directory. This should contain the file arrayobject.h, among others.
-export NUMPY_INCLUDE_PATH=/usr/lib/pymodules/python2.7/numpy/core/include/numpy
+export NUMPY_INCLUDE_PATH=/usr/lib64/python2.6/site-packages/numpy/core/include/numpy
 
 # ATLAS library directory. This should contain the file libcblas.so, among others.
-export ATLAS_LIB_PATH=/usr/lib/atlas-base
+export ATLAS_LIB_PATH=/usr/lib64/atlas
 
 make $*
 
